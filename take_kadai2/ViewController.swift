e2f3c43c78826ba0ac5bcf5ebab0e22327ffc18c
aliceTake <alicemaga7787@gmail.com>
2021-11-12 21:57:23 +0900

    フィードバック修正

--- take_kadai2/ViewController.swift
+++ take_kadai2/ViewController.swift
@@ -8,15 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet private weak var textField1: UITextField! // 上のtextField
-    @IBOutlet private weak var textField2: UITextField! // 下のtextField
+    @IBOutlet private weak var topTextField: UITextField! // 上のtextField
+    @IBOutlet private weak var bottomTextField: UITextField! // 下のtextField
     @IBOutlet private weak var segmentedControl: UISegmentedControl!
     @IBOutlet private weak var resultLabel: UILabel!
 
     @IBAction private func tappedButton(_ sender: Any) {
-        let num1 = Double(textField1.text ?? "") ?? 0
-        let num2 = Double(textField2.text ?? "") ?? 0
+        let num1 = Double(topTextField.text ?? "") ?? 0
+        let num2 = Double(bottomTextField.text ?? "") ?? 0
 
         switch segmentedControl.selectedSegmentIndex {
         case 0:
@@ -26,7 +25,7 @@ class ViewController: UIViewController {
         case 2:
             resultLabel.text = String(num1 * num2)
         case 3:
-            if num1 == 0 || num2 == 0 {
+            if num2 == 0 {
                 resultLabel.text = "割る数には0以外を入力してください"
             } else {
                 resultLabel.text = String(num1 / num2)
